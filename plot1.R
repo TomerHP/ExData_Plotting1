57a129e6a9c95e2387d1e7577ec78c17eb960063
TomerHP <tomerhp@gmail.com>
2016-10-06 16:20:01 +0300

    06/10/16/16:19

--- plot1.R
+++ plot1.R
@@ -1,2 +1,8 @@
 file<-read.csv("household_power_consumption.txt")
 df<-sqldf("select * from file where Date='1/2/2007'OR Date='2/2/2007'")
+
+##Plot 1
+df$Global_active_power<-as.numeric(levels(df$Global_active_power))[df$Global_active_power]
+hist(df$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
+
+##Plot 2
